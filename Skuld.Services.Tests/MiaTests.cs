709edf95dc81acc48b0edd327aae813b4ade4d8c
exsersewo <exsersewo@systemexit.co.uk>
2020-06-20 13:34:05 +0100

    Add Mia Tests, move non game stuff from cmd tree

---
+++ Skuld.Services.Tests/MiaTests.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using Skuld.Core.Extensions;
+using Skuld.Services.Gambling;
+using Xunit;
+
+namespace Skuld.Services.Tests
+{
+    public class MiaTests
+    {
+        [Fact]
+        public static void ReRollTest()
+        {
+            Assert.Equal(250ul, MiaHandler.GetAmountFromReRolls(1000, 1));
+            Assert.Equal(500ul, MiaHandler.GetAmountFromReRolls(1000, 2));
+            Assert.Equal(750ul, MiaHandler.GetAmountFromReRolls(1000, 3));
+        }
+
+        [Fact]
+        public static void WritingDiceTest()
+        {
+            var dies = new Dice(2)
+                .SetDieValue(0, 5)
+                .SetDieValue(1, 5);
+
+            Assert.Equal("||?, ?||", MiaHandler.GetRollString(dies, true));
+            Assert.Equal("5, 5", MiaHandler.GetRollString(dies, false));
+        }
+    }
+}
--- Skuld.Services/Gambling/MiaHandler.cs
+++ Skuld.Services/Gambling/MiaHandler.cs
@@ -1,15 +1,43 @@
 ﻿using Discord;
+using Skuld.Bot.Models.GamblingModule;
+using Skuld.Core.Extensions;
 using Skuld.Services.Exceptions;
 using Skuld.Services.Gambling.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace Skuld.Services.Gambling
 {
     public static class MiaHandler
     {
-        static List<MiaSession> Sessions = new List<MiaSession>();
+        static readonly List<MiaSession> Sessions = new List<MiaSession>();
         public const ushort MaxReRolls = 5;
+        private static readonly List<ushort[]> miaValues
+                = new List<ushort[]>
+            {
+                new ushort[] { 2, 1 },
+                new ushort[] { 6, 6 },
+                new ushort[] { 5, 5 },
+                new ushort[] { 4, 4 },
+                new ushort[] { 3, 3 },
+                new ushort[] { 2, 2 },
+                new ushort[] { 1, 1 },
+                new ushort[] { 6, 5 },
+                new ushort[] { 6, 4 },
+                new ushort[] { 6, 3 },
+                new ushort[] { 6, 2 },
+                new ushort[] { 6, 1 },
+                new ushort[] { 5, 4 },
+                new ushort[] { 5, 3 },
+                new ushort[] { 5, 2 },
+                new ushort[] { 5, 1 },
+                new ushort[] { 4, 3 },
+                new ushort[] { 4, 2 },
+                new ushort[] { 4, 1 },
+                new ushort[] { 3, 2 },
+                new ushort[] { 3, 1 }
+            };
 
         public static MiaSession NewSession(IUser user, 
             ulong betAmount, 
@@ -31,7 +59,8 @@ namespace Skuld.Services.Gambling
                     Amount = betAmount,
                     BotDice = botDice,
                     PlayerDice = playerDice,
-                    PreviousMessage = previousMessage
+                    PreviousMessage = previousMessage,
+                    ReRolls = 0
                 };
 
                 Sessions.Add(session);
@@ -90,5 +119,57 @@ namespace Skuld.Services.Gambling
 
         public static bool IsSessionInProgress(IUser user)
             => Sessions.Any(x => x.Target == user.Id);
+
+        public static ulong GetAmountFromReRolls(ulong Amount, ulong ReRolls)
+            => (ulong)(Amount * (ReRolls * 0.25));
+
+        public static string GetRollString(
+                Dice dies,
+                bool isHidden
+            )
+        {
+            StringBuilder str = new StringBuilder(isHidden ? "||" : "");
+
+            Die[] dieArr = dies
+                .GetDies()
+                .OrderByDescending(x => x.Face)
+                .ToArray();
+
+            if (!isHidden)
+            {
+                str.Append(string.Join(", ", dieArr.Select(x => x.Face.ToString()).ToArray()));
+            }
+            else
+            {
+                str.Append(string.Join(", ", dieArr.Select(x => x.Face.As("?")).ToArray()));
+            }
+
+            str.Append(isHidden ? "||" : "");
+
+            return str.ToString();
+        }
+
+        public static WinResult DidPlayerWin(Die[] bot, Die[] player)
+        {
+            var botDies = bot
+                .OrderByDescending(x => x.Face)
+                .ToArray();
+            var playerDies = player
+                .OrderByDescending(x => x.Face)
+                .ToArray();
+
+            var botIndex = miaValues
+                .FindIndex(x =>
+                    x[0] == botDies[0] && x[1] == botDies[1]
+                );
+            var playerIndex = miaValues
+                .FindIndex(x =>
+                    x[0] == playerDies[0] && x[1] == playerDies[1]
+                );
+
+            if (playerIndex < botIndex) return WinResult.PlayerWin;
+            else if (botIndex < playerIndex) return WinResult.BotWin;
+            else return WinResult.Draw;
+        }
     }
 }
